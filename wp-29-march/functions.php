025c564bec4a816455f5a5af42d275017c0a476b
MuhammadLahin <muhammad.lahin@gmail.com>
2018-04-19 00:08:39 +0600

    Update functions.php

--- wp-29-march/functions.php
+++ wp-29-march/functions.php
@@ -9,8 +9,10 @@ add_theme_support('widgets');
 function myScriptFiles() {
     //style
     wp_enqueue_style('font-google-api', 'http://fonts.googleapis.com/css?family=Nova+Mono');
-    wp_enqueue_style('all-css', 'https://use.fontawesome.com/releases/v5.0.9/css/all.css');
+    wp_enqueue_style('font-awesome-css', 'https://use.fontawesome.com/releases/v5.0.9/css/all.css');
     wp_enqueue_style('bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
+    wp_enqueue_style('style-css', get_template_directory_uri().'/assets/css/style.css');
+    wp_enqueue_style('style-wp', get_template_directory_uri());
 }
 
 add_action('wp_enqueue_scripts', 'myScriptFiles');
