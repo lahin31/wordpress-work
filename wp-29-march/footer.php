1be0e435a1e644b055321a4b9dd4b1cfaa931076
MuhammadLahin <muhammad.lahin@gmail.com>
2018-04-19 00:12:58 +0600

    Update footer.php

--- wp-29-march/footer.php
+++ wp-29-march/footer.php
@@ -2,7 +2,6 @@
 <p>&copy; Untitled. All rights reserved. Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
 </div>
 <!-- end #footer -->
-<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
