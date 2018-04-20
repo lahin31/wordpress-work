5a3cb61f070885ccf63f82fb80f5e8b889e6e1ec
MuhammadLahin <muhammad.lahin@gmail.com>
2018-04-20 14:01:37 +0600

    Update footer.php

--- wp-28-march/footer.php
+++ wp-28-march/footer.php
@@ -83,11 +83,6 @@
                 </div>
             </div>
         </footer>
-
-        <!-- load JS files -->
-        <script src="<?php echo get_template_directory_uri();?>/assets/js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
-        <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script> <!-- Tether for Bootstrap, http://stackoverflow.com/questions/34567939/how-to-fix-the-error-error-bootstrap-tooltips-require-tether-http-github-h --> 
-        <script src="<?php echo get_template_directory_uri();?>/assets/js/bootstrap.min.js"></script>                 <!-- Bootstrap (http://v4-alpha.getbootstrap.com/) -->
 </body>
 <?php wp_footer(); ?>
 </html>
